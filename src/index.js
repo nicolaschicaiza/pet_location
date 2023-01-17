cdb2dd46224bed3cfea0d6db240721b4cb2d96ed
Richard Sanchez <caicedorich@unicauca.edu.co>
2023-01-16 21:56:29 -0500

    autenticacion de usuarios con login

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from '@auth0/auth0-react';
+
+const domain = 'dev-j4hlmv8yfs1o8so8.us.auth0.com';
+const clientId = 'f9E0RfK8LDuF4gY74HI65NlkIeXwB7ci';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+     domain={domain} 
+    clientId={clientId}
+    redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>  
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
