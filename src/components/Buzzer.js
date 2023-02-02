06c5e6d97880371294d9604cb7bf91de0eef87fe
Richard Sanchez <caicedorich@unicauca.edu.co>
2023-02-01 23:47:28 -0500

    Boton de buzzer consulta a thingspeak y hace los cambios

--- src/components/Buzzer.js
+++ src/components/Buzzer.js
@@ -1,5 +1,6 @@
-import React ,{ useState}from 'react';
+import React, { useState, useEffect } from "react";
 import { FaPowerOff } from "react-icons/fa";
+import { getDataFromThingSpeak } from "../services/ThingSpeakService";
 import "./Buzzer.css";
 
 //metodo post con react y fetch links de ayuda:
@@ -7,53 +8,81 @@ import "./Buzzer.css";
 //https://stackblitz.com/edit/react-http-post-request-examples-fetch?file=App%2FApp.jsx
 
 const Buzzer = () => {
-    const [valueBuzzer, setValueBuzzer] = useState(false);
+  const [data, setData] = useState([]);
+  // let markers = [];
+  require("dotenv").config();
 
-        const apagar = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ api_key:process.env.REACT_APP_THINGSPEAK_WRITEKEY,
-                  field5: 0 })
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      const channelId = process.env.REACT_APP_THINGSPEAK_CHANNEL_ID;
+      const readKey = process.env.REACT_APP_THINGSPEAK_READKEY;
+      const response = await getDataFromThingSpeak(channelId, readKey);
+      const res = await response.json();
+      console.log(res.feeds);
+      setData(res.feeds);
+    };
+    fetchData();
+  }, []);
 
-        const encender = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ api_key:process.env.REACT_APP_THINGSPEAK_WRITEKEY,
-                  field5: 1 })
-        };
-        const off = () => {
-            fetch('https://api.thingspeak.com/update.json', apagar)
-            // .then(response => response.json())
-            // .then(data => this.setState({ postId: data.id }));
-        }
+  const estados = [];
 
-        const on = () => {
-            fetch('https://api.thingspeak.com/update.json', encender)
-            // .then(response => response.json())
-            // .then(data => this.setState({ postId: data.id }));
-        }
-        const onChangeValue = () => {
-            setValueBuzzer(!valueBuzzer);
-            if (valueBuzzer) {
-                fetch("https://api.thingspeak.com/update.json", encender);
-            } else {
-                fetch("https://api.thingspeak.com/update.json", apagar);
-            }
-        };
+  let cons = data.length - 1;
+  if (cons > 0) {
+    for (let i = cons; i < data.length; i++) {
+      //guardados todos
+      estados[i] = parseInt(data[i].field5);
 
-        return (
-            <div className="container-button">
-            <button
-                onClick={onChangeValue}
-                className={`btn btn--off ${valueBuzzer && "btn--on"}`}
-            >
-                <FaPowerOff />
-            </button>
-        </div>
-            
-        );
-    // }
-}
+      console.log("Estados: " + estados[i]);
+    }
+  }
 
-export {Buzzer}; 
+  const [valueBuzzer, setValueBuzzer] = useState(false);
+
+  const apagar = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      api_key: process.env.REACT_APP_THINGSPEAK_WRITEKEY,
+      field5: 0,
+    }),
+  };
+
+  const encender = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      api_key: process.env.REACT_APP_THINGSPEAK_WRITEKEY,
+      field5: 1,
+    }),
+  };
+  let estado1 = -1;
+  const onChangeValue = () => {
+    if (estados[cons] == 0) {
+      estado1 = 1;
+    } else {
+      estado1 = 0;
+    }
+    if (estado1 == 1) {
+      fetch("https://api.thingspeak.com/update.json", encender);
+    } else {
+      fetch("https://api.thingspeak.com/update.json", apagar);
+    }
+  };
+
+  console.log("estado1 " + estado1);
+  return (
+    <div className="container-button">
+      <button
+        onClick={onChangeValue}
+        className={`btn   ${estados[cons] == 0 && "btn--off"} ${
+          estados[cons] == 1 && "btn--on"
+        } `}
+      >
+        <FaPowerOff />
+      </button>
+    </div>
+  );
+  // }
+};
+
+export { Buzzer };
